ec503ed9e878d841ff4e51f2101c197f01d00cc1
bastidiaaz <basty.diaaz@gmail.com>
2019-01-15 04:39:13 +0100

    :construction: Structure for states

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -24,17 +24,24 @@
 		// This is required for Browser Sync to work poperly
 		$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-		
-		$urlRouterProvider
-			.otherwise('/dashboard');
-		
+		$urlRouterProvider.otherwise('/');
+		// $stateProvider
+		// 	.state('home', {
+		// 		url: '/',
+		// 		templateUrl: 'public/partials/home-partial.html',
+		// 		controller: 'HomeController'
+		// 	})
+		// 	.state('about', {
+		// 		url: '/about',
+		// 		templateUrl: 'public/partials/about-partial.html',
+		// 		controller: 'AboutController'
+		// 	});
 	}
 
 	runBlock.$inject = ['$rootScope'];
 
 	function runBlock($rootScope) {
 		'use strict';
-
 		console.log('AngularJS run() function...');
 	}
 
