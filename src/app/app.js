168dcd054dec26b2643ed21554fa73656c898c85
bastidiaaz <basty.diaaz@gmail.com>
2019-01-15 04:38:19 +0100

    :wip: Allow to use vm as controller on components

--- src/app/app.js
+++ src/app/app.js
@@ -10,19 +10,22 @@
 	* Main module of the application.
 	*/
 
-	angular.module('comgress', [
+	var app = angular.module('comgress', [
 		'ngResource',
 		'ngAria',
-		 'ui.bootstrap',
-		 'ngMaterial',
+		'ui.bootstrap',
+	  'ngMaterial',
 		'ngMdIcons',
 		'ngMessages',
 		'ngCookies',
 		'ngAnimate',
 		'ngTouch',
 		'ngSanitize',
-		'ui.router',
-		'home',
+		'ui.router'
 	]);
 
+	var _component = app.component;
+	app.component = function (name, options) {
+		return _component(name, angular.extend({ controllerAs: 'vm' }, options));
+	};
 })();
